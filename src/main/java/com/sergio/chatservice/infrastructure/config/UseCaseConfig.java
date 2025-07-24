d0754cc4874d0aef8a2866eb8cf6ee28ee6b53f6
Sergio Duran <538ser@gmail.com>
2025-07-24 14:01:13 -0600

    Created the bean for the ContextUseCase

--- src/main/java/com/sergio/chatservice/infrastructure/config/UseCaseConfig.java
+++ src/main/java/com/sergio/chatservice/infrastructure/config/UseCaseConfig.java
@@ -1,6 +1,8 @@
 package com.sergio.chatservice.infrastructure.config;
 
+import com.sergio.chatservice.application.usecase.ContextUseCase;
 import com.sergio.chatservice.application.usecase.MessageUseCase;
+import com.sergio.chatservice.domain.repository.ContextInfoRepository;
 import com.sergio.chatservice.domain.repository.MessageRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,10 @@ public class UseCaseConfig {
     @Bean
     public MessageUseCase messageUseCase(MessageRepository messageRepository) {
         return new MessageUseCase(messageRepository);
+    }
+
+    @Bean
+    public ContextUseCase contextUseCase(ContextInfoRepository contextInfoRepository) {
+        return new ContextUseCase(contextInfoRepository);
     }
 }
